254bcba5eee4986b7017292986e6edbde7d6e0db
Khánh Quỳnh <khanhquynh@MacBook-Air-cua-Khanh.local>
2025-12-25 20:21:01 +0700

    Nop bai 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import postRouter from '#routes/posts.route.js';
-import commentRouter from '#routes/comment.routes.js';
+import commentRouter from '#routes/comments.route.js';
 
 const router = express.Router();
 router.use('/posts', postRouter);
--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { getAllPosts, getPostById, createPostHandler, updatePostById, deletePostById } from '#controllers/posts.controller.js';
 import {validateData} from '#middlewares/validateData.middleware.js';
 import {createPostSchema, updatePostSchema} from '#schemas/post.schema.js';
-import commentRouter from '#routes/comment.routes.js';
+import commentsRouter from '#routes/comments.route.js';
 
 const router = express.Router();
 
-router.use('/:postId/comments', commentRouter);
+router.use('/:postId/comments', commentsRouter);
 router.get('/:postId', getPostById);
 router.get('/', getAllPosts);
 router.post('/', validateData(createPostSchema), createPostHandler);
